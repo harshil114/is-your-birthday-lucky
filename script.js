cb4072133472e0337be7349cc29b7c898fd3d082
harshil114 <harshilvispute114@gmail.com>
2023-05-20 13:43:32 +0530

    return sum

---
+++ script.js
@@ -0,0 +1,24 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+
+function checkBirthDateIsLuky(){
+    const dob = dateOfBirth.value;
+    const sum= calculateSum(dob);
+    console.log(sum);
+
+}
+
+function calculateSum(dob){
+
+    dob=dob.replaceAll("-","");
+    let sum=0;
+    for(let i=0; i<dob.length; i++)
+    {
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+
+}
+
+checkNumberButton.addEventListener('click',checkBirthDateIsLuky)
